760daa812e2ee7053f49c308e887c7151fcf2458
Daniel070793 <daniel070793@gmail.com>
2018-01-10 13:08:37 +0100

    faste øverførsler

--- MonedaClient/Moneda.UI/Viewmodels/DashboardViewmodel.cs
+++ MonedaClient/Moneda.UI/Viewmodels/DashboardViewmodel.cs
@@ -12,16 +12,44 @@ namespace Moneda.UI.Viewmodels
     {
         private ObservableCollection<Account> _accounts;
         private Account _selectedAccount;
+        private ObservableCollection<CashFlow> _fixedCashFlows;
+        private CashFlow _selectedCashFlow;
 
         public DashboardViewmodel()
         {
+            _fixedCashFlows = new ObservableCollection<CashFlow>();
             _accounts = new ObservableCollection<Account>();
             ObservableCollection<CashFlow> _transactions = new ObservableCollection<CashFlow>();
-            _transactions.Add(new CashFlow { Amount = 1000, Description = "Jomfru ane gade" });
-            _transactions.Add(new CashFlow { Amount = -1000000m, Description = "Bajerekkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" });
+            _transactions.Add(new CashFlow { Frequency = FrequencyEnum.Weekly, Amount = 1000m, Description = "Jomfru ane gade" });
+            _transactions.Add(new CashFlow { Frequency = FrequencyEnum.Single, Amount = -1000000m, Description = "Bajerekkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" });
 
             _accounts.Add(new Account { Name = "Opsparing", Balance = 500, Transactions = _transactions });
-            _accounts.Add(new Account { Name = "Lønkonto", Balance = 250.5m });
+            _accounts.Add(new Account { Name = "Lønkonto", Balance = 250.5m });       
+
+        }
+
+        private void SortCashflows()
+        {
+            if(_selectedAccount.Transactions != null)
+            {
+                _fixedCashFlows.Clear();
+                foreach (var cash in _selectedAccount.Transactions)
+                {
+                    if (cash.Frequency != FrequencyEnum.Single)
+                    {
+                        _fixedCashFlows.Add(cash);
+                    }
+                }
+            }                       
+        }
+
+        public CashFlow SelectedCashFlow
+        {
+            get { return _selectedCashFlow; }
+            set
+            {
+                _selectedCashFlow = value;
+            }
         }
 
         public Account SelectedAccount
@@ -30,7 +58,16 @@ namespace Moneda.UI.Viewmodels
             set
             {
                 _selectedAccount = value;
+                SortCashflows();
                 OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<CashFlow> FixedCashFlows
+        {
+            get { return _fixedCashFlows; }
+            set
+            {
+                _fixedCashFlows = value;
             }
         }
         public ObservableCollection<Account> Accounts
